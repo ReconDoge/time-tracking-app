a9cdd555d0f8ca1328392a929c15e16f4a260f29
ReconDoge <30973229+ReconDoge@users.noreply.github.com>
2021-08-04 18:32:09 +1000

    Add files via upload

--- timerAppController.py
+++ timerAppController.py
@@ -1,5 +1,9 @@
+import timerAppAnalytics as analytics
 
 import pandas as pd
+
+
+plt = analytics.Plotter(r"timerdatabase.xlsx")
 
 
 def convert_into_df(data: dict, index=[0]):
@@ -13,13 +17,16 @@ def save_df(df):
 
     f = r"timerdatabase.xlsx"
     df.to_excel(f)
+    
+
+def row_data(row: int):
+    data = [d for d in plt.get_data(row)]
+    return data
 
 
-def read_df(f):
+def header_names():
+    names = [col for col in plt.columns]
+    return names
 
-    df = pd.read_excel(f)
-
-    return df
-
-
-
+print(header_names())
+print(row_data(0))
